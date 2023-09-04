01601c5ea0969f1ce4736827977da768f2323b38
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-09-04 14:38:58 +0200

    HobbyWithPersonCount

--- src/main/java/dat/data/dao/HobbyDAO.java
+++ src/main/java/dat/data/dao/HobbyDAO.java
@@ -1,7 +1,11 @@
 package dat.data.dao;
 
 import dat.data.dao.boilerplate.DAO;
+import dat.data.dto.HobbyWithPersonCountDTO;
 import dat.model.Hobby;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
 
 public class HobbyDAO extends DAO<Hobby> {
 
@@ -15,5 +19,14 @@ public class HobbyDAO extends DAO<Hobby> {
         }
 
         return instance;
+    }
+
+    public List<HobbyWithPersonCountDTO> getAllHobbiesWithPersonCount() {
+        try (EntityManager em = super.getEntityManagerFactory().createEntityManager()) {
+            return em.createQuery("SELECT new dat.data.dto.HobbyWithPersonCountDTO(h, COUNT(p)) FROM Hobby h LEFT JOIN PersonHobby ph ON h.id = ph.hobby.id LEFT JOIN Person p ON ph.person.id = p.id GROUP BY h.id", HobbyWithPersonCountDTO.class).getResultList();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            return null;
+        }
     }
 }
---
+++ src/main/java/dat/data/dto/HobbyWithPersonCountDTO.java
@@ -0,0 +1,5 @@
+package dat.data.dto;
+
+import dat.model.Hobby;
+
+public record HobbyWithPersonCountDTO(Hobby hobby, long count) { }
