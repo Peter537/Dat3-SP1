377c7db56a0da309ad4608225a93041238424012
Yusuf Al-Khafaji <GLBcraft1234@gmail.com>
2023-09-04 10:07:59 +0200

    Address and zip

---
+++ src/main/java/dat/model/Zip.java
@@ -0,0 +1,26 @@
+package dat.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Zip {
+
+    @Id
+    @Column(name = "zip")
+    private int zip;
+    @Column(name = "city_name")
+    private String city_name;
+    @Column(name = "state_name")
+    private String region_name;
+    @Column(name = "municipality_name")
+    private String municipality_name;
+
+
+
+}
