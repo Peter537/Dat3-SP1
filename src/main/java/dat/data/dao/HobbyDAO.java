93e678cbc3b91fc28482f51a320009fdade2306b
MagKon <magnus.kongstad@gmail.com>
2023-09-04 14:36:31 +0200

    Update HobbyDAO.java

--- src/main/java/dat/data/dao/HobbyDAO.java
+++ src/main/java/dat/data/dao/HobbyDAO.java
@@ -1,7 +1,12 @@
 package dat.data.dao;
 
+import dat.config.HibernateConfig;
 import dat.data.dao.boilerplate.DAO;
 import dat.model.Hobby;
+import dat.model.HobbyType;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
 
 public class HobbyDAO extends DAO<Hobby> {
 
@@ -15,5 +20,20 @@ public class HobbyDAO extends DAO<Hobby> {
         }
 
         return instance;
+    }
+
+    public List<Hobby> getHobbiesByType(HobbyType hobbyType) {
+        try (EntityManager entityManager = super.getEntityManagerFactory().createEntityManager()) {
+            entityManager.getTransaction().begin();
+            List<Hobby> hobbies = entityManager.createQuery("SELECT h FROM Hobby h WHERE h.type = :hobbyType", Hobby.class)
+                    .setParameter("hobbyType", hobbyType)
+                    .getResultList();
+            entityManager.getTransaction().commit();
+            return hobbies;
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            return null;
+        }
     }
 }
